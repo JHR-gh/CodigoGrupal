7cae2dad5a6ffafb84272f64d1a9e0feeb2b52fb
JHR-gh <145732512+JHR-gh@users.noreply.github.com>
2023-09-22 09:10:40 -0500

    Add files via upload

---
+++ LecturaDeDatos.h
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int leedatoint()
+{
+	int dato;
+	cin>>dato;
+	return dato;
+}
+
+float leedatofloat()
+{
+	float dato;
+	cin>>dato;
+	return dato;
+}
+
+double leedatodouble()
+{
+	double dato;
+	cin>>dato;
+	return dato;
+}
+
+char leedatochar()
+{
+	char dato;
+	cin>>dato;
+	return dato;
+}
+
+string leedatostring()
+{
+	string dato;
+	cin>>dato;
+	return dato;
+}
+
+int pedirop()
+{
+	int op;
+	cout << "Ingrese una opción:\n";
+    cin >> op;
+	return op;
+}
